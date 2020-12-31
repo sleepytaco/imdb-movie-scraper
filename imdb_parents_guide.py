e614b3c777352918f4c6deb2530dfb9048959de3
Mohammed Abu Bakar Khan <abukhan363@gmail.com>
2020-12-31 03:58:38 -0600

    Update imdb_parents_guide.py

--- imdb_parents_guide.py
+++ imdb_parents_guide.py
@@ -5,7 +5,6 @@ import sys
 
 
 def main(movie=''):
-
     imdb = 'https://www.imdb.com/'
 
     ##### SPACER #####
@@ -26,19 +25,8 @@ def main(movie=''):
         for i in range(30):
             print()
 
-        ##### ROTTEN TOMATOES INFO #####
-        rotten_site = requests.get(f'https://www.rottentomatoes.com/m/{"_".join(movie.split(" "))}')
-        rotten_soup = BeautifulSoup(rotten_site.text, 'lxml')
-        try:
-            rotten_rating = rotten_soup.find_all('span', class_='mop-ratings-wrap__percentage')[0].text.strip()
-        except IndexError:
-            rotten_rating = 'N/A'
-        try:
-            rotten_crtitcs_census = \
-            rotten_soup.find_all('p', class_='mop-ratings-wrap__text mop-ratings-wrap__text--concensus')[
-                0].text.strip()
-        except IndexError:
-            rotten_crtitcs_census = 'N/A'
+        print(f"Gathering info for '{movie}'...")
+
 
         ##### IMDB SEARCH PAGE INFO #####
         site = requests.get(imdb + f'find?q={"+".join(movie.split(" "))}&ref_=nv_sr_sm')
@@ -46,6 +34,9 @@ def main(movie=''):
         try:
             movie_page = soup.find_all('td', class_='result_text')[0].find_all('a')[0].get('href')
         except IndexError:
+            ##### SPACER #####
+            for i in range(30):
+                print()
             print(f"Uh-oh! No such movie called '{movie}' was found!\n")
             g = input(f"Enter 1 to google '{movie}' or type another movie to search: ")
             if g == '1':
@@ -63,8 +54,12 @@ def main(movie=''):
         site = requests.get(imdb + movie_page)
         soup = BeautifulSoup(site.text, 'lxml')
         try:
-            movie_name_and_year = ' '.join(soup.find_all('div', class_='title_wrapper')[0].find_all('h1')[0].text.split('\xa0'))
+            movie_name_and_year = ' '.join(
+                soup.find_all('div', class_='title_wrapper')[0].find_all('h1')[0].text.split('\xa0'))
         except IndexError:
+            ##### SPACER #####
+            for i in range(30):
+                print()
             open_page = input(f"Looks like not much information about the movie was found!"
                               f"\nEnter 1 to google '{movie}' or type another movie to search: ")
 
@@ -87,10 +82,12 @@ def main(movie=''):
         except IndexError:
             movie_summary = 'N/A'
 
+        ##### SPACER #####
+        for i in range(30):
+            print()
+
         print("MOVIE:", movie_name_and_year)
         print("METASCORE:", metacritic_score)
-        print("ROTTEN TOMATOES:", rotten_rating)
-        print("CRITICS CENSUS:", rotten_crtitcs_census)
         print(f"IMDb SUMMARY: {movie_summary}")
 
         ##### PARENTS GUIDE PAGE INFO #####
@@ -99,7 +96,8 @@ def main(movie=''):
         soup = BeautifulSoup(site.text, 'lxml')
 
         try:
-            nudity_level = soup.find_all('section', {'id': 'advisory-nudity'})[0].find_all('li')[0].text.strip().split('\n')[0]
+            nudity_level = \
+            soup.find_all('section', {'id': 'advisory-nudity'})[0].find_all('li')[0].text.strip().split('\n')[0]
         except IndexError:
             nudity_level = "N/A"
         if len(soup.find_all('section', {'id': 'advisory-nudity'})[0].find_all('li')) == 1:
